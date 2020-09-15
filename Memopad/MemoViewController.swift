c2bb407931f8d4fe244856499ee01a0059ae5ae1
maya <amacrk@i.softbank.jp>
2020-09-15 22:05:21 +0900

    create memo

---
+++ Memopad/MemoViewController.swift
@@ -0,0 +1,68 @@
+//
+//  MemoViewController.swift
+//  Memopad
+//
+//  Created by maya on 2020/09/15.
+//  Copyright © 2020 maya. All rights reserved.
+//
+
+import UIKit
+
+class MemoViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet var titleTextField: UITextField!
+    @IBOutlet var contentTextView: UITextView!
+
+    var saveData: UserDefaults = UserDefaults.standard
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        titleTextField.text = saveData.object(forKey: "title") as? String
+        contentTextView.text = saveData.object(forKey: "content") as? String
+        titleTextField.delegate = self
+    }
+    
+    @IBAction func saveMemo() {
+        //書き込み
+        saveData.set(titleTextField.text, forKey: "title")
+        saveData.set(contentTextView.text, forKey: "content")
+        
+        //alert
+        let alert: UIAlertController = UIAlertController(
+            title: "保存",
+            message:"メモの保存が完了しました",
+            preferredStyle: .alert
+        )
+        //OKbutton
+        alert.addAction(
+            UIAlertAction(
+                title: "OK",
+                style: .default,
+                handler: { action in
+                    //ボタンが押された時の動作, 前の画面に戻る
+                    self.navigationController?.popViewController(animated: true)
+                    print("OKボタンが押されました")
+            }
+            )
+        )
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
